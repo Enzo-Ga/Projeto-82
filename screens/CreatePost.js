dfe0199c792878f8a2cf8b4abf6d523ebb32839a
Enzo-Ga <93016718+Enzo-Ga@users.noreply.github.com>
2022-12-29 21:51:55 -0300

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text, StyleSheet } from "react-native";
+
+export default class CreatePost extends Component {
+  render() {
+    return <Text>Create Post</Text>;
+  }
+}
